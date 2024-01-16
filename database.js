b1dbb25c84bf14a5d6c7085b15a0b103ca52e99e
Stiven Gomez <stivengomez@soyyo.co>
2024-01-16 15:59:04 -0500

    FIX - Ajuste de ssl.

--- database.js
+++ database.js
@@ -5,5 +5,5 @@ config();
 
 export const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: true // Descomentariar esto para local
+    // ssl: true // Descomentariar esto para local
 });
