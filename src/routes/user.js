e3af2d6dd013f88e6aca16cbc3258f67e33e4529
Stiven Gomez <stivengomez@soyyo.co>
2024-01-18 18:31:48 -0500

    FIX - Encriptado.

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,121 @@
-const express = require('express');
+import { Router } from 'express';
+import { pool } from '../../database.js';
 
-const router = express.Router();
+import crypto from 'crypto';
 
-router.get('/notifications', (req, res) => {
+export const userRouter = Router();
 
-    res.json();
+userRouter.get('/login', (req, res) => {
+    // const name = 'Stiven Gomez';
+    // const email = 'stivenalexander7u7@gmail.com';
+    // const password = 'TheSuperankes15!';
+
+
+    // console.log("-----------------------------------------");
+    // console.log("INFORMACIÓN");
+
+    // console.log("Nombre: " + name + "\nEmail: " + email + "\nContraseña: " + password);
+
+    // console.log("-----------------------------------------");
+
+
+    // console.log("INFORMACIÓN ENCRIPTADA");
+    // var passwordEncript = encrypt(password);
+    // // console.log(passwordEncript.encryptedData);
+    // console.log("Nombre: " + name + "\nEmail: " + email + "\nContraseña: " + passwordEncript.encryptedData);
+    
+    // console.log("-----------------------------------------");
+    
+    // console.log("INFORMACIÓN DESENCRIPTADA");
+    // var passwordDescript = decrypt(passwordEncript);
+    // console.log("Nombre: " + name + "\nEmail: " + email + "\nContraseña: " + passwordDescript);
+
+
+    // const crypto = require('crypto');
+
+    const algorithm = 'aes-256-cbc';
+    const key = crypto.randomBytes(32);
+    const iv = crypto.randomBytes(16);
+
+    const password = 'TheSuperankes15!'
+
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+    let encrypted = cipher.update(password, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+
+    const decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+    let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+
+    console.log('Original text: ', password);
+    console.log('Encrypted text: ', encrypted);
+    console.log('Decrypted text: ', decrypted);
+
+    res.json({ username: 'Stiven Gomez'});
 });
 
-module.exports = router;
+userRouter.post('/register', (req, res) => {
+
+    const { id, name, isactive } = req.body;
+
+    // --------------------------------
+        // CUENTA - USUARIO
+
+        // Id - PK
+        // Name
+        // Email
+        // Phone
+        // isactive
+
+    
+    // --------------------------------
+        // HISTORIA LDE COMPRAS
+
+        // Id - PK
+        // Date
+        // TypePay
+        // TypePay
+        // amount
+
+
+
+
+    // --------------------------------
+
+    // --------------------------------
+    
+        // PRODUCTOS
+        
+        // Id - PK
+        // idCategory - FK
+        // Name
+        // Description
+        // Price
+        // Img
+        // isavailable 1 - 0
+        // amount
+    
+    // --------------------------------
+
+
+    // --------------------------------
+    
+        // CATEGORIA
+        
+        // Id - PK
+        // Name
+        // Price
+        // Img
+        // isavailable 1 - 0
+        // amount
+    
+    // --------------------------------
+
+
+
+
+
+    res.json({ username: 'Stiven Gomez'});
+});
